bbc1310b346defc9a3b0370cded703737dd1e048
aymak91 <amakyu@gmail.com>
2024-03-01 01:43:53 -0800

    1337code

--- index.js
+++ index.js
@@ -1,38 +1,45 @@
 const projectNames = {
   1: {
-    name: "pokecord [1/5]",
+    name: "pokecord [1/6]",
     live: "https://pokecord-ruby.vercel.app",
     repo: "https://github.com/aymak91/pokecord",
     technologies: "React, Redux, Firebase, Google Authentication, Tailwind CSS, HTML",
     description: "Discord clone with a Pokemon aesthetic!"
   },
   2: {
-    name: "Pathfinder [2/5]",
+    name: "Pathfinder [2/6]",
     live: "https://aymak91.github.io/pathfinding_visualizer/",
     repo: "https://github.com/aymak91/pathfinding_visualizer",
     technologies: "Javascript, HTML, CSS",
     description: "Interactive pathfinding visualization using the breadth-first search graph traversal algorithm."
   },
   3: {
-    name: "woofblr [3/5]",
+    name: "woofblr [3/6]",
     live: "https://woofblr.herokuapp.com/#/",
     repo: "https://github.com/aymak91/woofblr",
     technologies: "React, Redux, Ruby on Rails, Postgres, AWS S3, HTML, CSS",
     description: "Woof! woofblr is a full-stack web-application clone of Tumblr, except for dogs! Users can create various types of posts such as text and images. A dashboard displays content posted by other users that a user follows."
   },
   4: {
-    name: "BoneMeatsKibble [4/5]",
+    name: "BoneMeatsKibble [4/6]",
     live: "https://bone-meats-kibble.herokuapp.com",
     repo: "https://github.com/aymak91/bone-meats-kibble",
     technologies: "Javascript, React, Node.js, Express, MongoDB, AWS S3, HTML, CSS",
     description: "BoneMeatsKibble is a MERN stack project where you can add profiles for your beloved canines and find matches for them. Find a pal for your pup, or perhaps something more!"
   },
   5: {
-    name: "Hungry Wolf [5/5]",
+    name: "Hungry Wolf [5/6]",
     live: "https://aymak91.github.io/hungry-wolf",
     repo: "https://github.com/aymak91/hungry-wolf",
     technologies: "Javascript, Canvas, HTML, CSS",
     description: "Hungry Wolf is a JavaScript game where you play Wolfey, a young pooch who has found a juicy piece of meat on a farm but now must avoid capture by the angry local farmers. How many farmers can you scare away before capture?!"
+  },
+  6: {
+    name: "1337code [6/6]",
+    live: "",
+    repo: "",
+    technologies: "Javascript, Canvas, HTML, CSS",
+    description: ""
   },
 }
 
